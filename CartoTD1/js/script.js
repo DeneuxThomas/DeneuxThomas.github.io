d8af9f4c39c53fe0bc4dca7b813f84bed31a741a
Thomas DENEUX <thomas.deneuxrahil@gmail.com>
2023-09-12 10:50:12 +0200

    getCurrentPosition et t watchPosition

--- CartoTD1/js/script.js
+++ CartoTD1/js/script.js
@@ -1,19 +1,34 @@
 if ("geolocation" in navigator) {
     navigator.geolocation.getCurrentPosition(function(position) {
         
-        const longitude = position.coords.longitude;
-        const latitude = position.coords.latitude;
-        const altitude = position.coords.altitude;
-        const precision = position.coords.accuracy;
-        const vitesse = position.coords.speed;
-        const timestamp = new Date(position.timestamp);
+        const getCurrentPositionlongitude = position.coords.longitude;
+        const getCurrentPositionlatitude = position.coords.latitude;
+        const getCurrentPositionaltitude = position.coords.altitude;
+        const getCurrentPositionprecision = position.coords.accuracy;
+        const getCurrentPositionvitesse = position.coords.speed;
+        const getCurrentPositiontimestamp = new Date(position.timestamp);
 
-        document.getElementById("longitude").textContent = longitude;
-        document.getElementById("latitude").textContent = latitude;
-        document.getElementById("altitude").textContent = altitude;
-        document.getElementById("precision").textContent = precision;
-        document.getElementById("vitesse").textContent = vitesse;
-        document.getElementById("date").textContent = timestamp.toLocaleString();
+        document.getElementById("getCurrentPositionlongitude").textContent = getCurrentPositionlongitude;
+        document.getElementById("getCurrentPositionlatitude").textContent = getCurrentPositionlatitude;
+        document.getElementById("getCurrentPositionaltitude").textContent = getCurrentPositionaltitude;
+        document.getElementById("getCurrentPositionprecision").textContent = getCurrentPositionprecision;
+        document.getElementById("getCurrentPositionvitesse").textContent = getCurrentPositionvitesse;
+        document.getElementById("getCurrentPositiondate").textContent = getCurrentPositiontimestamp.toLocaleString();
+    });
+    navigator.geolocation.watchPosition(function(position) {
+        const watchPositionlongitude = position.coords.longitude;
+        const watchPositionlatitude = position.coords.latitude;
+        const watchPositionaltitude = position.coords.altitude;
+        const watchPositionprecision = position.coords.accuracy;
+        const watchPositionvitesse = position.coords.speed;
+        const watchPositiontimestamp = new Date(position.timestamp);
+
+        document.getElementById("watchPositionlongitude").textContent = watchPositionlongitude;
+        document.getElementById("watchPositionlatitude").textContent = watchPositionlatitude;
+        document.getElementById("watchPositionaltitude").textContent = watchPositionaltitude;
+        document.getElementById("watchPositionprecision").textContent = watchPositionprecision;
+        document.getElementById("watchPositionvitesse").textContent = watchPositionvitesse;
+        document.getElementById("watchPositiondate").textContent = watchPositiontimestamp.toLocaleString();
     });
 } else {
     alert("La géolocalisation n'est pas prise en charge par votre navigateur.");
