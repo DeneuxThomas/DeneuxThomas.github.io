54080571618e4b6696b7cf4a963849a79eaa1b98
Thomas DENEUX <thomas.deneuxrahil@gmail.com>
2023-09-12 10:43:23 +0200

    retour arrière

--- CartoTD1/js/script.js
+++ CartoTD1/js/script.js
@@ -1,32 +1,20 @@
-// Vérifie si le navigateur prend en charge la géolocalisation
 if ("geolocation" in navigator) {
-    // Utilise getCurrentPosition() pour obtenir les données de géolocalisation actuelles
     navigator.geolocation.getCurrentPosition(function(position) {
-        displayPosition("current", position);
-    });
-
-    // Utilise watchPosition() pour suivre les changements de position en temps réel
-    const watchId = navigator.geolocation.watchPosition(function(position) {
-        displayPosition("watch", position);
-    });
-    
-    // Fonction pour afficher les données de géolocalisation
-    function displayPosition(prefix, position) {
+        
         const longitude = position.coords.longitude;
         const latitude = position.coords.latitude;
         const altitude = position.coords.altitude;
         const precision = position.coords.accuracy;
-        const speed = position.coords.speed;
+        const vitesse = position.coords.speed;
         const timestamp = new Date(position.timestamp);
 
-        document.getElementById(`${prefix}Lon`).textContent = longitude;
-        document.getElementById(`${prefix}Lat`).textContent = latitude;
-        document.getElementById(`${prefix}Alt`).textContent = altitude;
-        document.getElementById(`${prefix}Precision`).textContent = precision;
-        document.getElementById(`${prefix}Speed`).textContent = speed;
-        document.getElementById(`${prefix}Date`).textContent = timestamp.toLocaleString();
-    }
+        document.getElementById("longitude").textContent = longitude;
+        document.getElementById("latitude").textContent = latitude;
+        document.getElementById("altitude").textContent = altitude;
+        document.getElementById("precision").textContent = precision;
+        document.getElementById("vitesse").textContent = vitesse;
+        document.getElementById("date").textContent = timestamp.toLocaleString();
+    });
 } else {
-    // Si la géolocalisation n'est pas prise en charge, affiche un message d'erreur
     alert("La géolocalisation n'est pas prise en charge par votre navigateur.");
 }
