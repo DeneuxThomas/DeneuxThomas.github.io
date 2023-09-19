623e311f7c1c579ae1c65e9d6157e4839fc36eb3
Thomas DENEUX <thomas.deneuxrahil@gmail.com>
2023-09-19 11:59:44 +0200

    exo3

--- CartoTD2/Exo2/js/script.js
+++ CartoTD2/Exo2/js/script.js
@@ -7,8 +7,9 @@ L.tileLayer('http://tile.stamen.com/watercolor/{z}/{x}/{y}.jpg', {
 let myLatitude = 43.6174848;
 let myLongitude = 7.0811648;
 
-let marseilleCoordinates = [43.2965, 5.3698];
-let niceCoordinates = [43.7000, 7.2650];
+let myCoordinates = L.latLng(43.6174848, 7.0811648);
+let marseilleCoordinates = L.latLng(43.2965, 5.3698);
+let niceCoordinates = L.latLng(43.7000, 7.2650);
 
 let segment = L.polyline([marseilleCoordinates, niceCoordinates], {
     color: 'green',
@@ -32,28 +33,7 @@ let circle = L.circle([myLatitude, myLongitude], {
     radius: precision,
 }).addTo(map);
 
-function calculateDistance(lat1, lon1, lat2, lon2) {
-    let R = 6371;
-    let dLat = (lat2 - lat1) * (Math.PI / 180);
-    let dLon = (lon2 - lon1) * (Math.PI / 180);
-    let a =
-        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-        Math.cos(lat1 * (Math.PI / 180)) *
-        Math.cos(lat2 * (Math.PI / 180)) *
-        Math.sin(dLon / 2) * Math.sin(dLon / 2);
-    let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    let distance = R * c;
-    return distance;
-}
-
-let distanceMarseilleToYou = calculateDistance(
-    marseilleCoordinates[0],
-    marseilleCoordinates[1],
-    myLatitude,
-    myLongitude
-);
+let distance = marseilleCoordinates.distanceTo(myCoordinates) / 1000;
 
 let divDistance = document.getElementById('distance');
-divDistance.innerHTML = "<p>Distance entre Marseille et votre position: <span id='distance'>" + distanceMarseilleToYou.toFixed(2) + "</span> mètres</p>";
-
-console.log('Distance entre Marseille et votre position : ' + distanceMarseilleToYou.toFixed(2) + ' km');
+divDistance.innerHTML = "<p>Distance entre Marseille et votre position: <span id='distance'>" + distance.toFixed(2) + "</span> kilomètres</p>";
