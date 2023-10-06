e3122ca5ed8dcb39c9ae3fa44f61340cac2607be
Thomas DENEUX <thomas.deneuxrahil@gmail.com>
2023-10-06 17:48:14 +0200

    ajout d'un moai

--- CartoTD3/Exo2/js/script2.js
+++ CartoTD3/Exo2/js/script2.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -11,7 +12,7 @@ const geometry = new THREE.BoxGeometry(1, 1, 1);
 
 // Charger la texture
 const textureLoader = new THREE.TextureLoader();
-const texture = textureLoader.load('../img/raimbow.jpg');
+const texture = textureLoader.load('../ressource/raimbow.jpg');
 
 // Créer un matériau avec la texture
 const material = new THREE.MeshPhongMaterial({ map: texture });
@@ -19,16 +20,31 @@ const material = new THREE.MeshPhongMaterial({ map: texture });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-const light = new THREE.AmbientLight(0xffffff, .8);
-scene.add(light)
+const loader = new GLTFLoader();
+
+loader.load('../ressource/moai.glb', (gltf) => {
+    const model = gltf.scene;
+
+    model.position.set(10, -1, 0);
+    model.scale.set(1, 1, 1);
+    model.rotation.set(0, 0, 0);
+
+    scene.add(model);
+});
+
+
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 5);
+directionalLight.position.set(0, 5, 10);
+scene.add(directionalLight);
 
 camera.position.z = 4;
 
 function animate() {
     requestAnimationFrame(animate);
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    cube.rotation.x += 0.02;
+    cube.rotation.y += 0.02;
 
     renderer.render(scene, camera);
 }
